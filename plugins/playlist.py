f966caa10b86d71dbf7d4e2793d55101f96841c7
HeLLxGodLike <ysonu4951@gmail.com>
2022-03-16 23:43:14 +0530

    Update playlist.py

--- plugins/playlist.py
+++ plugins/playlist.py
@@ -7,9 +7,9 @@ from pyrogram.types import (
     Message,
 )
 
-from driver.decorators import check_blacklist
-from driver.queues import QUEUE, get_queue
-from driver.filters import command, other_filters
+from base.decorators import check_blacklist
+from base.queues import QUEUE, get_queue
+from base.filters import command, other_filters
 
 
 keyboard = InlineKeyboardMarkup(
